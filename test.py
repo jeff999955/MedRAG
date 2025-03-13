620c1436d33b1bae69acd78756ba4a10f5b9bf49
Jeff Tseng <jeff999955@gmail.com>
2025-03-13 15:53:33 -0700

    add testing script

---
+++ test.py
@@ -0,0 +1,13 @@
+from src.medrag import MedRAG
+
+question = "A lesion causing compression of the facial nerve at the stylomastoid foramen will cause ipsilateral"
+options = {
+    "A": "paralysis of the facial muscles.",
+    "B": "paralysis of the facial muscles and loss of taste.",
+    "C": "paralysis of the facial muscles, loss of taste and lacrimation.",
+    "D": "paralysis of the facial muscles, loss of taste, lacrimation and decreased salivation."
+}
+
+medrag = MedRAG(llm_name="OpenAI/gpt-3.5-turbo-16k", rag=True, retriever_name="MedCPT", corpus_name="PubMed")
+answer, snippets, scores = medrag.answer(question=question, options=options, k=32) # scores are given by the retrieval system
+print(f"Final answer in json with rationale: {answer}")
